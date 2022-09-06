aa431b4fcbd83a3e3b628feca5beaf313f27a53c
dan1aa <burmeidan@gmail.com>
2022-09-06 11:55:26 +0300

    features

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,6 @@
 window.onload = async function () {
   chrome.storage.local.set({ "background": "#08AB67" })
-  const parsed_URL = window.location.href.split('/').slice(-1).toString();
-  const id = uid();
+  const parsed_URL = window.location.href.split('/').slice(-1).toString().slice(0, 12);
 
   function $el(tag, props) {
     let p, el = document.createElement(tag);
@@ -103,6 +102,7 @@ window.onload = async function () {
     <div class="talk-time-top" title="Click to collapse/expand">
       <img class="talk-time-logo" style=" filter: grayscale(1) invert(1);" src="https://cdn-icons-png.flaticon.com/24/1827/1827379.png" />
       <button class="show-statistic">Show statistic</button>
+      <a class="feedback-a" style="color: white !important; font-size: 14px;" href="https://extension-node-api.herokuapp.com/feedbacks/${parsed_URL}">Feedbacks</a>
       <svg class="talk-time-options-gear" title="Talk Time Options" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>
     </div>
     <div class="talk-time-options-content">
@@ -428,6 +428,30 @@ window.onload = async function () {
             }
           }
         })
+    })
+  }, 5000)
+
+  setInterval(() => {
+    let allNames = document.querySelectorAll('.zWGUib');
+    let allImgUrls = document.querySelectorAll('.KjWwNd');
+    let users = [];
+    for(let i = 0; i < allNames.length; i++) {
+      users.push({
+        name: allNames[i].textContent,
+        url: parsed_URL,
+        img: allImgUrls[i].getAttribute('src')
+      })
+    }
+    fetch(`https://extension-node-api.herokuapp.com/feedbacks/${parsed_URL}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Token': 'Bearer 580792'
+      },
+      body: JSON.stringify({
+        users
+      })
     })
   }, 5000)
 
